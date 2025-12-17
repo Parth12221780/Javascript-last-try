2a07fae93c84e38b68b6fe94a0665387f5e5e55c
parth <parth82073@gmail.com>
2025-12-18 03:04:41 +0530

    i learned about the variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 55
+let accountEmail = "parth122@gmai.com"
+var accountPassword = "321"
+accountCity = "Los Angeles"
+// accountId = 8 // const directly not allowed
+// const accountId = 9 // it can not be re write
+
+// console.log(accountId)
+
+// jab hame multiple console .log use krnna ho tb hm console.table ka use krte hai 
+
+accountEmail = 5
+accountPassword = "1234"
+accountCity = "New York"
+
+// do not use var now a days
+
+console.table({ accountId, accountEmail, accountPassword, accountCity })
